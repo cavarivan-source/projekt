7f8e536b80a1ef03af73cd9ce0aec94908fc9003
cavarivan <cavar.ivan@gmail.com>
2026-01-12 19:53:11 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import Test from './components/Test';
+import Novitest from './components/Novitest';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<Test/>
+<Novitest/>
+<h2>Drugi naslov</h2>
+
+
+</>
   );
 }
 
---
+++ src/components/Novitest.js
@@ -0,0 +1,14 @@
+import "./Novitest.css"
+const Novitest =() => {
+    return (
+        <>
+           <div className ="newtest">Evo testiram</div>
+           <div className ="oldtest">Test i dalje</div>
+           </>
+     
+        
+
+    )
+}
+
+export default Novitest;
